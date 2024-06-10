8b25356d00b106137783facce0d8bb6154c234fc
Harley Pascua <pascua@greenova.de>
2024-06-10 05:39:43 +0200

    introduce upload worker

---
+++ src/Globot.Web/App/Services/GlobUploadWorker.cs
@@ -0,0 +1,54 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Globot.Web.Configuration;
+using Microsoft.Extensions.FileSystemGlobbing;
+using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
+
+namespace Globot.Web.App.Services;
+
+public class GlobUploadWorker
+{
+    private readonly GlobotConfiguration _globot;
+    private readonly ILogger _log;
+    private readonly string _knownSourceName;
+
+    public GlobUploadWorker(GlobotConfiguration globot, ILogger log, string knownSourceName)
+    {
+        _globot = globot;
+        _log = log;
+        _knownSourceName = knownSourceName;
+    }
+
+    public async Task UploadGlobs(CancellationToken cancelToken)
+    {
+        var knownSource = _globot.KnownSources[_knownSourceName];
+
+        var matcher = new Matcher();
+        var includedPatterns = knownSource.FileExtensions
+            .Select(fe => $"**/{fe}")
+            .ToArray();
+
+        matcher.AddIncludePatterns(includedPatterns);
+        
+        var sourceDir = new DirectoryInfo(knownSource.Path!);
+        var globs = matcher.Execute(new DirectoryInfoWrapper(sourceDir));
+
+        if (!globs.HasMatches)
+        {
+            _log.LogInformation("No files found in path '{Path}'", knownSource.Path);
+            return;
+        }
+
+        var container = GetBlobContainer();
+
+    }
+
+    private BlobContainerClient GetBlobContainer()
+    {
+        var clientConf = _globot.Azure.BlobServiceClient!;
+
+        var storageContainer = new BlobContainerClient(clientConf.ConnectionString, clientConf.ContainerName);
+
+        return storageContainer;
+    }
+}
---
+++ src/Globot.Web/Configuration/GlobotConfigurationExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Globot.Web.Configuration;
+using Globot.Web.App.Services;
+
+namespace Globot.Web;
+
+public static class GlobotConfigurationExtensions
+{
+    public static IEnumerable<GlobUploadWorker> CreateGlobUploadWorkers(this GlobotConfiguration globot, ILoggerFactory logFactory)
+    {
+        var _ = logFactory.CreateLogger("GlobotConfigurationExtensions");
+
+        foreach (string knownSourceName in globot.KnownSources.Keys)
+        {
+            var (worker, err) = CreateGlobUploadWorker(globot, logFactory, knownSourceName);
+            if (err != null)
+            {
+                _.LogWarning("Unable to create GlobUploadWorker instance for [{knownSourceName}]. Reason: {message}", knownSourceName, err.Message);
+                continue;
+            }
+
+            yield return worker!;
+        }
+    }
+
+    public static (GlobUploadWorker?, Exception?) CreateGlobUploadWorker(this GlobotConfiguration globot, ILoggerFactory logFactory, string knownSourceName)
+    {
+        if (!globot.KnownSources.ContainsKey(knownSourceName))
+        {
+            return (null, new ArgumentOutOfRangeException("Invalid KnownSource key: " + knownSourceName));
+        }
+
+        var knownSource = globot.KnownSources[knownSourceName];
+
+        if (!Directory.Exists(knownSource.Path))
+        {
+            return (null, new DirectoryNotFoundException($"THe path '{knownSource.Path}' for source '{knownSourceName}' cannot be located."));
+        }
+
+        var log = logFactory.CreateLogger($"{typeof(GlobUploadWorker).FullName}.{knownSourceName}");
+
+        return (new GlobUploadWorker(globot, log, knownSource), null);
+    }
+}
